43e430f0dfd8b59e9b20e352d29280a37bb87bd9
James <james.mcgrory135@gmail.com>
2023-03-20 13:38:25 +1100

    added project colours and typography from Figma file to tailwindconfig

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div>
-      
+      <h1 className='text-7xl'>test</h1>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'peach': '#E7816B',
+        'light-peach': '#FFAD9B',
+        'black': '#1D1C1E',
+        'dark-grey': '#333136',
+        'light-grey': '#F1F3F5'
+      },
+      fontFamily: {
+        'jost' : ['Jost', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
