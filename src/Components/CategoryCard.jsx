891accbebb391449cc8c2083b59051fbbe20897e
James <james.mcgrory135@gmail.com>
2023-03-21 11:15:43 +1100

    category card section is now tablet and desktop responsive

--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -2,21 +2,35 @@ import React from "react";
 
 export default function CategoryCard(props) {
     return (
-        <div className="mx-6 bg-black flex flex-col items-center justify-center gap-y-3 h-64 rounded-2xl relative overflow-hidden">
+        <div className="mx-6 bg-black flex flex-col items-center justify-center gap-y-3 h-full rounded-2xl relative overflow-hidden md:mx-10 min-[1191px]:mx-0">
+
+            {/* Mobile background image */}
             <img
-                className="absolute h-full w-full mix-blend-overlay" 
-                src={props.img} 
-                alt="" 
+                className="absolute h-full w-full mix-blend-overlay md:invisible" 
+                src={props.mobileImg} 
             />
-            <h3 className="text-white text-[1.75rem] leading-9 tracking-[0.0875rem] uppercase relative">
+            {/* Tablet background image */}
+            <img
+                className="absolute h-full w-full mix-blend-overlay invisible md:visible lg:invisible" 
+                src={props.tabletImg} 
+            />
+            {/* Desktop background image */}
+            <img
+                className="absolute h-full w-full mix-blend-overlay invisible lg:visible" 
+                src={props.desktopImg} 
+            />
+
+            <h3 className="text-white text-[1.75rem] font-medium leading-9 tracking-[0.0875rem] uppercase relative md:text-[2.5rem] md:tracking-[0.125rem] md:leading-10 md:mb-4">
                 {props.title}
             </h3>
+
             <div className="flex items-center gap-x-4 relative">
                 <p className="text-white tracking-[0.3125rem] uppercase">
                     View Projects
                 </p>
                 <img src="/shared/desktop/icon-right-arrow.svg" alt="" />
             </div>
+
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,10 +6,33 @@ export default function Home() {
     return (
         <section>
             <Hero />
-            <div className="mt-32 flex flex-col gap-y-6">
-                <CategoryCard title={"web design"} img={'/home/mobile/image-web-design.jpg'}/>
-                <CategoryCard title={"app design"} img={'/home/mobile/image-app-design.jpg'}/>
-                <CategoryCard title={"graphic design"} img={'/home/mobile/image-graphic-design.jpg'}/>
+            <div className="mt-32 flex flex-col gap-y-6 max-w-[69.4375rem] mx-auto lg:flex-row lg:justify-center lg:gap-8">
+                <div className="lg:w-[33.8125rem] h-64 md:h-52 lg:h-[40rem]">
+                    <CategoryCard 
+                        title={"web design"} 
+                        mobileImg={'/home/mobile/image-web-design.jpg'}
+                        tabletImg={'/home/tablet/image-web-design.jpg'}
+                        desktopImg={'/home/desktop/image-web-design-large.jpg'}
+                    />
+                </div>
+                <div className="flex flex-col gap-y-6 lg:w-[33.8125rem]">
+                    <div className="lg:w-[33.8125rem] h-64 md:h-52 lg:h-[19.25rem]">
+                        <CategoryCard 
+                            title={"app design"} 
+                            mobileImg={'/home/mobile/image-app-design.jpg'}
+                            tabletImg={'/home/tablet/image-app-design.jpg'}
+                            desktopImg={'/home/desktop/image-app-design.jpg'}
+                        />
+                    </div>
+                    <div className="lg:w-[33.8125rem] h-64 md:h-52 lg:h-[19.25rem]">
+                        <CategoryCard 
+                            title={"graphic design"} 
+                            mobileImg={'/home/mobile/image-graphic-design.jpg'}
+                            tabletImg={'/home/tablet/image-graphic-design.jpg'}
+                            desktopImg={'/home/desktop/image-graphic-design.jpg'}
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     )
