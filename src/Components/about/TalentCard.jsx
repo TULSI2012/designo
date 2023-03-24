3a2b11156b9badd0825f138bf7ed7c2faeda9015
James <james.mcgrory135@gmail.com>
2023-03-24 20:41:38 +1100

    refactored the about header section into its own component for readability + finished talent section fully responsive

---
+++ src/Components/about/TalentCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function TalentCard() {
+    return (
+            <section className=" bg-[#FDF3F0] pb-20 bg-[url('/shared/desktop/bg-pattern-three-circles.svg')] bg-[left_20rem] bg-no-repeat md:mx-10 md:mt-28 md:bg-[center_top_12rem] lg:flex lg:h-[40rem] lg:items-center lg:pb-0 md:rounded-2xl lg:bg-[right_12rem_bottom]">
+                <picture>
+                    <source media="(min-width:1440px)" srcSet="/about/desktop/image-world-class-talent.jpg" />
+                    <source media="(min-width:768px)" srcSet="/about/tablet/image-world-class-talent.jpg" />
+                    <img src="/about/mobile/image-world-class-talent.jpg" className="w-full md:rounded-t-2xl lg:rounded-r-none lg:rounded-l-2xl"/>
+                </picture>
+                <div className="lg:ml-24">
+                    <h1 className="text-[2rem] text-peach text-center font-medium mt-20 mb-6 md:text-[2.5rem] md:mt-16 lg:text-left">
+                        World-class talent
+                    </h1>
+                    <p className="text-center leading-6 mx-6 max-w-xl md:mx-auto lg:text-left lg:max-w-md">
+                        We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, 
+                        ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.   
+                        <br/><br />
+                        Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. 
+                        We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality 
+                        outcomes that encapsulates their brand’s story and mission.
+                    </p>
+                </div>
+            </section>
+    )
+}
+
+//picture tag can be used with srcSet to display different images for different screen sizes
+// todo: clean up how i previously handled different images on different screen sizes with css hidden and display block classes
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,26 +1,12 @@
 import React from "react";
+import AboutHeader from "../Components/about/AboutHeader";
+import TalentCard from "../Components/about/TalentCard";
 
 export default function About() {
     return (
         <section className="max-w-[74.45rem] mx-auto">
-            <div className=" bg-peach text-white pb-20 bg-[url('/about/mobile/bg-pattern-hero-about-mobile.svg')] bg-[right_bottom_-27rem] bg-no-repeat md:mx-10 md:rounded-2xl md:bg-[url('/about/desktop/bg-pattern-hero-about-desktop.svg')] md:bg-left lg:flex lg:flex-row-reverse lg:h-[30rem] lg:items-center lg:pb-0 lg:justify-between">
-                <picture>
-                    <source media="(min-width:1440px)" srcSet="/about/desktop/image-about-hero.jpg" />
-                    <source media="(min-width:768px)" srcSet="/about/tablet/image-about-hero.jpg" />
-                    <img src="/about/mobile/image-about-hero.jpg" className="w-full mb-8 md:rounded-t-2xl lg:rounded-l-none lg:rounded-r-2xl lg:mb-0"/>
-                </picture>
-                <div className="lg:ml-24">
-                    <h1 className="text-[2rem] text-center font-medium mb-6 md:text-5xl lg:text-left">
-                        About Us
-                    </h1>
-                    <p className="text-center leading-6 mx-6 max-w-xl md:mx-auto lg:text-left lg:max-w-md">
-                        Founded in 2010, we are a creative agency that produces lasting results for our clients. 
-                        We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that 
-                        make real impact. We’re always looking forward to creating brands, products, and digital experiences 
-                        that connect with our clients’ audiences.
-                    </p>
-                </div>
-            </div>
+            <AboutHeader />
+            <TalentCard />
         </section>
     )
 }
