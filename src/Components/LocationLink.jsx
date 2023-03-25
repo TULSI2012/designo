0fbfdde79a602824fac712487afc3de9c3f4c3df
James <james.mcgrory135@gmail.com>
2023-03-25 14:16:33 +1100

    added location icon section to about page, fully responsive and hover states

---
+++ src/Components/LocationLink.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function LocationLink(props) {
+    return (
+        <div className="flex flex-col items-center">
+            <div className="bg-[url('/shared/desktop/bg-pattern-small-circle.svg')] mb-12">
+                <img src={props.img} alt="canada illustration" />
+            </div>
+            <h3 className="text-xl font-medium uppercase tracking-[0.3125rem] text-center mb-8">
+                {props.country}
+            </h3>
+            <button className="bg-peach text-white uppercase font-medium tracking-[0.0625rem] rounded-lg p-4 cursor-pointer hover:bg-light-peach transition-all duration-200 ease-in-out">
+                See Location
+            </button>
+        </div>
+    )
+}
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import AboutHeader from "../Components/about/AboutHeader";
 import TalentCard from "../Components/about/TalentCard";
+import LocationLink from "../Components/LocationLink";
 
 export default function About() {
     return (
         <section className="max-w-[74.45rem] mx-auto">
             <AboutHeader />
             <TalentCard />
+            <section className="flex flex-col gap-y-12 my-32 min-[1000px]:flex-row min-[1000px]:justify-center min-[1000px]:gap-x-40">
+                <LocationLink country={"canada"} img={"/shared/desktop/illustration-canada.svg"}/>
+                <LocationLink country={"australia"} img={"/shared/desktop/illustration-australia.svg"}/>
+                <LocationLink country={"united kingdom"} img={"/shared/desktop/illustration-united-kingdom.svg"}/>
+            </section>
         </section>
     )
 }
